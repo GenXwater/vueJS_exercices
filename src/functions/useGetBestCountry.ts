56b7d234fb0c6d12b4503875e5640c570c50b7c9
GenXwater <louis.genty@outlook.com>
2026-03-05 16:58:28 +0100

    feat: implement useGetBestCountry function to determine the country with the highest value

---
+++ src/functions/useGetBestCountry.ts
@@ -0,0 +1,21 @@
+import useFormatRevenues from "./useFormatRevenues";
+
+export default function useGetBestCountry(i: any) {
+    const itemCopy = { ...i};
+    delete itemCopy.data;
+    delete itemCopy.app;
+    delete itemCopy.totalRevenue;
+    delete itemCopy.totalViews;
+    delete itemCopy.totalConversions;
+    let bestCountryValue = 0;
+    let bestCountryName = "";
+
+    for (const property in itemCopy) {
+        if (itemCopy[property] > bestCountryValue) {
+            bestCountryValue = itemCopy[property];
+            bestCountryName = property
+        }
+    }
+
+    return `${bestCountryName.slice(2)} with a total of ${useFormatRevenues(bestCountryValue)}`;
+}
