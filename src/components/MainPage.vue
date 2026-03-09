3c3856990f71e8ad37b50bdb3be3bafb7ac15066
GenXwater <louis.genty@outlook.com>
2026-03-09 15:51:05 +0100

    feat: integrate recette store into MainPage component

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,7 +1,10 @@
 <template>
     Page
+    {{ storeRecette }}
 </template>
 
 <script setup lang="ts">
+    import { useRecetteStore } from '@/stores/recette';
 
+    const storeRecette = useRecetteStore()
 </script>
--- src/stores/recette.ts
+++ src/stores/recette.ts
@@ -1,7 +1,6 @@
 import { defineStore } from 'pinia'
 
-export const useRecetteStore = defineStore({
-    id: 'recette',
+export const useRecetteStore = defineStore('recette', {
 
     // State / Etat
     state: () => ({
