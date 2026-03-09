30dd3d5495b00f5e3d3c18a00d670a7a2c19e555
GenXwater <louis.genty@outlook.com>
2026-03-09 22:12:55 +0100

    Add SimpleButton component and update article and index pages to use it

--- app/pages/blog/article1.vue
+++ app/pages/blog/article1.vue
@@ -2,5 +2,5 @@
     <h1>Article 1</h1>
     <p>ozdeoizdoizjedoized oizedoijzdoized zedoizedoized zedizjed zedoizjedz edizedoizejd zedozedijzed</p>
     <NuxtLink to="/blog">Liste des articles</NuxtLink><br /><br />
-    <NuxtLink to="/">Accueil</NuxtLink>
+    <SimpleButton><NuxtLink to="/">Accueil</NuxtLink></SimpleButton>
 </template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,7 @@
 <template>
     <h1>Page d'accueil</h1>
     <p>ozdeoizdoizjedoized oizedoijzdoized zedoizedoized zedizjed zedoizjedz edizedoizejd zedozedijzed</p>
-    <NuxtLink to="/contact">Contact</NuxtLink><br />
-    <NuxtLink to="/blog">Blog</NuxtLink>
+    <NuxtLink to="/contact">Contact</NuxtLink><br /><br />
+    <NuxtLink to="/blog">Blog</NuxtLink><br /><br />
+    <SimpleButton>Mon Btn</SimpleButton>
 </template>
