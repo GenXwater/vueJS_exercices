ace5a340ba78388aa9a70d90af15764f5ec37410
GenXwater <louis.genty@outlook.com>
2026-03-09 22:55:20 +0100

    Add copyright image to index page and update layout

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,5 +3,7 @@
     <p>ozdeoizdoizjedoized oizedoijzdoized zedoizedoized zedizjed zedoizjedz edizedoizejd zedozedijzed</p>
     <NuxtLink to="/contact">Contact</NuxtLink><br /><br />
     <NuxtLink to="/blog">Blog</NuxtLink><br /><br />
-    <SimpleButton>Mon Btn</SimpleButton>
+    <SimpleButton>Mon Btn</SimpleButton><br />
+    <img src="/copyright-6114655_1280-3092848910.jpg" width="220" /> <!-- depuis /public -->
+    <img src="~/assets/copyright-6114655_1280-3092848910.jpg" width="220" /> <!-- depuis /assets -->
 </template>
