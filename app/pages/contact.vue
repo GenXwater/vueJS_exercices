0cb30fe22fbfe1407cb39d9fbdcc4341c1b16fe9
GenXwater <louis.genty@outlook.com>
2026-03-09 22:22:39 +0100

    Add custom layout for contact page and update page metadata

--- app/pages/contact.vue
+++ app/pages/contact.vue
@@ -3,3 +3,9 @@
     <p>Utilisez ce formulaire pour envoyer votre message</p>
     <NuxtLink to="/">Accueil</NuxtLink>
 </template>
+
+<script setup lang="ts">
+    definePageMeta({
+        layout: `custom` //permet explicitement de choisir le layout souhaité (donc plus de header ni de footer en l'occurrence)
+    })
+</script>
