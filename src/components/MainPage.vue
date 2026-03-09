85a50dec4580c63eaf02e24c56f138e2b3b3f417
GenXwater <louis.genty@outlook.com>
2026-03-09 15:57:26 +0100

    feat: update MainPage component to display recettes list

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,10 +1,14 @@
 <template>
-    Page
-    {{ storeRecette }}
+    <h1>Page</h1>
+    {{ recettes }}
+    <ul>
+        <li v-for="r in recettes">{{ r.name  }}</li>
+    </ul>
 </template>
 
 <script setup lang="ts">
     import { useRecetteStore } from '@/stores/recette';
 
     const storeRecette = useRecetteStore()
+    const recettes = storeRecette.getRecettes
 </script>
