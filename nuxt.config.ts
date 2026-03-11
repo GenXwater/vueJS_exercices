763e258a301a22228ba9ea84ddd57fc7d888fed1
GenXwater <louis.genty@outlook.com>
2026-03-11 15:38:21 +0100

    Add animate.css CDN link to nuxt.config.ts for global CSS inclusion

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,11 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
+  app: {
+    head: {
+      link: [{ rel: 'stylesheet', href:`https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css`}]
+    }
+  }, // ça c'est pour directement injecter le CSS sans isntallation npm intall, mais moins fiable car dépendance réseau externe.
+  
   css: [`~/assets/test.css`, `animate.css/`] /* une autre façon d'inclure un style css au global, sinon voir index.vue */
 })
