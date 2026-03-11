5e57626abdfb1d8734459588992e3c5db10b845a
GenXwater <louis.genty@outlook.com>
2026-03-11 19:12:56 +0100

    Refactor blog articles: remove static article1.vue and implement dynamic article-[id].vue for better scalability

--- app/pages/blog/index.vue
+++ app/pages/blog/index.vue
@@ -1,5 +1,7 @@
 <template>
     <h1>Liste des articles</h1>
-    <NuxtLink to="/blog/article1">Article 1</NuxtLink><br /><br />
+    <ul>
+        <li v-for="i in 5"><NuxtLink :to="`/blog/article-`+i">Article {{ i }}</NuxtLink></li>
+    </ul>>
     <NuxtLink to="/">Accueil</NuxtLink>
 </template>
