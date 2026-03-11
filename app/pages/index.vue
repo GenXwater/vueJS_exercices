3b873589e1812e951e2b07eaa49ff04a4781b4ff
GenXwater <louis.genty@outlook.com>
2026-03-11 18:51:21 +0100

    Add useHead to index.vue for animate.css global inclusion

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -8,6 +8,13 @@
     <img class="animate__animated animate__headShake" src="~/assets/copyright-6114655_1280-3092848910.jpg" width="220" /> <!-- depuis /assets -->
 </template>
 
+<script setup lang="ts">
+    useHead({
+        link: [{ rel: 'stylesheet', href:`https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css`}]
+    })
+    // encore un autre façon d'injecter la classe CCS, mais juste pour la page concernée.
+</script>
+
 <style>
 /* @import url("~/assets/test.css");    <---- Une façon d'insérer du style */
 </style>
