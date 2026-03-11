a160bcbbd7bb11ca8ebbb4c572bc55ede7969436
GenXwater <louis.genty@outlook.com>
2026-03-11 22:29:16 +0100

    feat: add default layout with navbar and footer

---
+++ app/layouts/default.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <header>
+            <!-- Fixed navbar -->
+            <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">Fixed navbar</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
+                    <div class="collapse navbar-collapse" id="navbarCollapse">
+                        <ul class="navbar-nav me-auto mb-2 mb-md-0">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Link</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
+        
+        <!-- Begin page content -->
+        <main class="flex-shrink-0">
+            <div id="mainContainer" class="container">
+                <slot />
+            </div>
+        </main>
+
+        <!-- Footer -->
+        <footer class="footer mt-auto py-3 bg-body-tertiary">
+            <div class="container">
+                <span class="text-body-secondary">Place sticky footer content here.</span>
+            </div>
+        </footer>
+    </div>>
+</template>
+
+<style>
+    #mainContainer {
+        margin-top: 80px;
+    }
+
+    footer {
+        position: fixed;
+        bottom: 0;
+        width: 100%;
+    }
+</style>
