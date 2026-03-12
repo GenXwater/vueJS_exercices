b97c8b3dbe31b919e992027f7b2aac0e2cbc6636
GenXwater <louis.genty@outlook.com>
2026-03-12 13:37:24 +0100

    feat: restructure index page layout and enhance news card display

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,15 +1,22 @@
 <template>
-  <div class="row align-items-start">
-    <div v-for="actu in data.results" class="card" style="width: 18rem;">
-      <img :src="actu.image_url" class="card-img-top" alt="">
-      <div class="card-body">
-        <h5 class="card-title">{{ actu.title }}</h5>
-        <p class="card-text">{{ actu.description }}</p>
-        <a :href="actu.link" class="btn btn-primary">Lire la suite</a>
+  <div class="container py-4">
+    <div class="row g-3">
+      <div
+        v-for="actu in (data?.results ?? [])"
+        :key="actu.link"
+        class="col-12 col-sm-6 col-lg-4"
+      >
+        <div class="card h-100 shadow-sm">
+          <img :src="actu.image_url" class="card-img-top" alt="">
+          <div class="card-body">
+            <h5 class="card-title">{{ actu.title.slice(0, 100) }}</h5>
+            <p class="card-text">{{ actu.description.slice(0, 300) }}</p>
+            <a :href="actu.link" class="btn btn-primary btn-sm">Lire la suite</a>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-      
+  </div>    
 </template>
 
 <script setup>
