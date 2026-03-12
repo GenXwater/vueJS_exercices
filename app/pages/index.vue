5574aebfd9a7f4a1a2b2db30a0f84565f0b4ac76
GenXwater <louis.genty@outlook.com>
2026-03-12 13:28:57 +0100

    feat: update index page to display news data in card format

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,11 +1,17 @@
 <template>
-  <div>
-    {{ data }}
+  <div class="row align-items-start">
+    <div v-for="actu in data.results" class="card" style="width: 18rem;">
+      <img :src="actu.image_url" class="card-img-top" alt="">
+      <div class="card-body">
+        <h5 class="card-title">{{ actu.title }}</h5>
+        <p class="card-text">{{ actu.description }}</p>
+        <a :href="actu.link" class="btn btn-primary">Lire la suite</a>
+      </div>
+    </div>
   </div>
+      
 </template>
 
 <script setup>
-const { data, pending, error } = await useFetch('/api/news', {
-  query: {}
-})
+const { data } = await useFetch('/api/news')
 </script>
