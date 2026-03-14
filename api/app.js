e92ca3663ee12c97cb7f4a046ede690d42bb1eb6
GenXwater <louis.genty@outlook.com>
2026-03-14 23:43:13 +0100

    Implement request handling for users, checkUser, and projects endpoints

--- api/app.js
+++ api/app.js
@@ -6,6 +6,25 @@ createServer(async (req, res) => {
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, Origin')
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
 
-    const url = new URL(req.url, `http://${req.headers.host}`)
-    console.log("url :", url)
-}).listen(3002)
+    const url = new URL(req.url, `http://${req.headers.host}`);
+    const method = req.method;
+    let response = JSON.stringify([]);
+
+    switch (url.pathname) {
+        case "/users":
+            console.log("users");
+        break;
+        case "/checkUser":
+
+        break;
+        case "/projects":
+
+        break;
+        default:
+            response = null;
+        break;
+    }
+
+    res.write(JSON.stringify(response));
+    res.end();
+}).listen(3002);
