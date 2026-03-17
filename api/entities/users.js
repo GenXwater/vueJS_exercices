71b7a88e667b644be78f86288a54862010884e8b
GenXwater <louis.genty@outlook.com>
2026-03-17 17:36:58 +0100

    Implement user management functions: addUser, deleteUserById, and checkUser

--- api/entities/users.js
+++ api/entities/users.js
@@ -11,4 +11,25 @@ export async function getUsers(hidePass = false) {
     } else {
         return JSON.parse(jsonData);
     }
-} 
+}
+
+export async function addUser({email, pass}) {
+    const newUser = 
+    { email, pass, isAdmin: false, id: crypto.randomUUID() }
+    const users = [newUser, ...await getUsers()];
+    await writeFile(path, JSON.stringify(users));
+    return newUser;
+}
+
+export async function deleteUserById(id) {
+    const users = await getUsers();
+    await writeFile(path, JSON.stringify(users.filter(u => u.id !== id)))
+    const newUsersList = await getUsers(true);
+    return newUsersList;
+}
+
+export async function checkUser(email, pass) {
+    const usersList = await getUsers();
+    const fondUser = usersList.filter(u => u.email === email && u.pass === pass);
+    return fondUser;
+}
