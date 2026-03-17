747e6983e2824d40a8593b6296a223f2e13a2ae2
GenXwater <louis.genty@outlook.com>
2026-03-17 18:05:10 +0100

    Add project management API functions: readProjects, createProject, and deleteProject

--- api/app.js
+++ api/app.js
@@ -1,5 +1,6 @@
 import { createServer } from "http";
 import { createUser, readUsers, deleteUser, checkUserLogin } from "./api/usersApi.js";
+import { createProject, deleteProject, readProjects } from "./api/projectsApi.js";
 
 createServer(async (req, res) => {
     res.setHeader('Content-Type', 'application/json')
@@ -25,7 +26,12 @@ createServer(async (req, res) => {
 
         break;
         case "/projects":
-
+            if (method === "GET")
+                response = await readProjects()
+            if (method === "POST")
+                response = await createProject(req)
+            if (method === "DELETE")
+                response = await deleteProject(url.searchParams.get('id'))
         break;
         default:
             response = null;
