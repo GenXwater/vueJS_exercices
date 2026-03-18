2732f7b388796b8ff38bd9622b392a0d275ba878
GenXwater <louis.genty@outlook.com>
2026-03-18 17:52:31 +0100

    Enhance menu button with avatar and update options list to include icons

--- vue_app_gestion/app/layouts/default.vue
+++ vue_app_gestion/app/layouts/default.vue
@@ -6,12 +6,27 @@
         <v-spacer />
         <v-menu>
           <template #activator="{ props }">
-            <v-btn icon="mdi-dots-vertical" variant="text" v-bind="props" />
+            <v-btn variant="text" v-bind="props" icon>
+              <v-avatar
+                image="https://robohash.org/vuejs16534215651894653168"
+                size="50"
+              />
+            </v-btn>
           </template>
 
-          <v-list>
-            <v-list-item v-for="(item, i) in items" :key="i" :value="i">
-              <v-list-item-title>{{ item.text }}</v-list-item-title>
+          <v-list class="pa-2 rounded-xl">
+            <v-list-item v-for="(item, i) in options"
+              :key="i"
+              :value="i"
+              color="primary"
+              rounded="xl"
+              :to="item.url"
+            >
+              <v-list-item-title v-text="item.text"></v-list-item-title>
+              
+              <template v-slot:append>
+                <v-icon :icon="item.icon"></v-icon>
+              </template>
             </v-list-item>
           </v-list>
         </v-menu>
@@ -63,5 +78,10 @@
     { text: 'Team', icon: 'mdi-account-group', url: "/team" },
     { text: 'Projects', icon: 'mdi-folder-multiple', url: "/projects" },
     { text: 'Reports', icon: 'mdi-file-chart', url: "/reports" },
+  ]
+
+  const options = [
+    { text: 'Options', icon: 'mdi-view-dashboard', url: '/options' },
+    { text: 'Se déconnecter', icon: 'mdi-account-group', url: "/team" },
   ]
 </script>
