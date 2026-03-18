9b1a2dfd6aa6861513631471d053f63439c8d295
GenXwater <louis.genty@outlook.com>
2026-03-18 22:20:28 +0100

    Refactor index page layout to improve user statistics display and update card titles

--- vue_app_gestion/app/pages/index.vue
+++ vue_app_gestion/app/pages/index.vue
@@ -1,96 +1,190 @@
 <template>
     <h1 class="ma-0 mb-4">Accueil</h1>
-    <v-card
-        class="mx-auto"
-        max-width="368"
-    >
-        <v-card-item title="Florida">
-        <template v-slot:subtitle>
-            <v-icon
-            class="me-1 pb-1"
-            color="error"
-            icon="mdi-alert"
-            size="18"
-            ></v-icon>
+    <v-container>
+        <v-row>
+            <v-col>
+                <v-card class="mx-auto rounded-xl pa-2" max-width="368">
+                    <v-card-item title="Utilisateurs">
+                    <template v-slot:subtitle>
+                        <v-icon
+                        class="me-1 pb-1"
+                        color="success"
+                        icon="mdi-trending-up"
+                        size="18"
+                        ></v-icon>
 
-            Extreme Weather Alert
-        </template>
-        </v-card-item>
+                        En hausse de 5%
+                    </template>
+                    </v-card-item>
 
-        <v-card-text class="py-0">
-        <v-row class="align-center" density="compact">
-            <v-col
-            class="text-display-large font-weight-light"
-            cols="6"
-            >
-            64&deg;F
+                    <v-card-text class="py-0">
+                    <v-row class="align-center" density="compact">
+                        <v-col
+                        class="text-display-large font-weight-light"
+                        cols="6"
+                        >
+                        64
+                        </v-col>
+
+                        <v-col class="text-right" cols="6">
+                        <v-icon
+                            color="error"
+                            icon="mdi-account"
+                            size="88"
+                        ></v-icon>
+                        </v-col>
+                    </v-row>
+                    </v-card-text>
+
+                    <div class="d-flex py-3 justify-space-between">
+                    <v-list-item
+                        density="compact"
+                        prepend-icon="mdi-gender-male"
+                    >
+                        <v-list-item-subtitle>34</v-list-item-subtitle>
+                    </v-list-item>
+
+                    <v-list-item
+                        density="compact"
+                        prepend-icon="mdi-gender-female"
+                    >
+                        <v-list-item-subtitle>30</v-list-item-subtitle>
+                    </v-list-item>
+                    </div>
+
+                    <v-expand-transition>
+                    <div v-if="expand">
+                        <div class="py-2">
+                        <v-slider
+                            v-model="time"
+                            :max="6"
+                            :step="1"
+                            :ticks="labels"
+                            class="mx-4"
+                            color="primary"
+                            density="compact"
+                            show-ticks="always"
+                            thumb-size="10"
+                            hide-details
+                        ></v-slider>
+                        </div>
+
+                        <v-list class="bg-transparent">
+                        <v-list-item
+                            v-for="item in forecast"
+                            :key="item.day"
+                            :append-icon="item.icon"
+                            :subtitle="item.temp"
+                            :title="item.day"
+                        >
+                        </v-list-item>
+                        </v-list>
+                    </div>
+                    </v-expand-transition>
+
+                    <v-divider></v-divider>
+
+                    <v-card-actions>
+                    <v-btn
+                        :text="'Détail'"
+                        :to="'/team'"
+                    ></v-btn>
+                    </v-card-actions>
+                </v-card>
             </v-col>
 
-            <v-col class="text-right" cols="6">
-            <v-icon
-                color="error"
-                icon="mdi-account"
-                size="88"
-            ></v-icon>
+            <v-col>
+                <v-card class="mx-auto rounded-xl pa-2" max-width="368">
+                    <v-card-item title="Utilisateurs">
+                    <template v-slot:subtitle>
+                        <v-icon
+                        class="me-1 pb-1"
+                        color="success"
+                        icon="mdi-trending-up"
+                        size="18"
+                        ></v-icon>
+
+                        En hausse de 5%
+                    </template>
+                    </v-card-item>
+
+                    <v-card-text class="py-0">
+                    <v-row class="align-center" density="compact">
+                        <v-col
+                        class="text-display-large font-weight-light"
+                        cols="6"
+                        >
+                        64
+                        </v-col>
+
+                        <v-col class="text-right" cols="6">
+                        <v-icon
+                            color="error"
+                            icon="mdi-account"
+                            size="88"
+                        ></v-icon>
+                        </v-col>
+                    </v-row>
+                    </v-card-text>
+
+                    <div class="d-flex py-3 justify-space-between">
+                    <v-list-item
+                        density="compact"
+                        prepend-icon="mdi-gender-male"
+                    >
+                        <v-list-item-subtitle>34</v-list-item-subtitle>
+                    </v-list-item>
+
+                    <v-list-item
+                        density="compact"
+                        prepend-icon="mdi-gender-female"
+                    >
+                        <v-list-item-subtitle>30</v-list-item-subtitle>
+                    </v-list-item>
+                    </div>
+
+                    <v-expand-transition>
+                    <div v-if="expand">
+                        <div class="py-2">
+                        <v-slider
+                            v-model="time"
+                            :max="6"
+                            :step="1"
+                            :ticks="labels"
+                            class="mx-4"
+                            color="primary"
+                            density="compact"
+                            show-ticks="always"
+                            thumb-size="10"
+                            hide-details
+                        ></v-slider>
+                        </div>
+
+                        <v-list class="bg-transparent">
+                        <v-list-item
+                            v-for="item in forecast"
+                            :key="item.day"
+                            :append-icon="item.icon"
+                            :subtitle="item.temp"
+                            :title="item.day"
+                        >
+                        </v-list-item>
+                        </v-list>
+                    </div>
+                    </v-expand-transition>
+
+                    <v-divider></v-divider>
+
+                    <v-card-actions>
+                    <v-btn
+                        :text="'Détail'"
+                        :to="'/team'"
+                    ></v-btn>
+                    </v-card-actions>
+                </v-card>
             </v-col>
         </v-row>
-        </v-card-text>
-
-        <div class="d-flex py-3 justify-space-between">
-        <v-list-item
-            density="compact"
-            prepend-icon="mdi-weather-windy"
-        >
-            <v-list-item-subtitle>123 km/h</v-list-item-subtitle>
-        </v-list-item>
-
-        <v-list-item
-            density="compact"
-            prepend-icon="mdi-weather-pouring"
-        >
-            <v-list-item-subtitle>48%</v-list-item-subtitle>
-        </v-list-item>
-        </div>
-
-        <v-expand-transition>
-        <div v-if="expand">
-            <div class="py-2">
-            <v-slider
-                v-model="time"
-                :max="6"
-                :step="1"
-                :ticks="labels"
-                class="mx-4"
-                color="primary"
-                density="compact"
-                show-ticks="always"
-                thumb-size="10"
-                hide-details
-            ></v-slider>
-            </div>
-
-            <v-list class="bg-transparent">
-            <v-list-item
-                v-for="item in forecast"
-                :key="item.day"
-                :append-icon="item.icon"
-                :subtitle="item.temp"
-                :title="item.day"
-            >
-            </v-list-item>
-            </v-list>
-        </div>
-        </v-expand-transition>
-
-        <v-divider></v-divider>
-
-        <v-card-actions>
-        <v-btn
-            :text="!expand ? 'Full Report' : 'Hide Report'"
-            @click="expand = !expand"
-        ></v-btn>
-        </v-card-actions>
-    </v-card>
+    </v-container>
 </template>
 
 
