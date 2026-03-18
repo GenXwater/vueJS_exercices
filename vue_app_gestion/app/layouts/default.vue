5816b5691f777b979743cf22bbf10395b28d7b35
GenXwater <louis.genty@outlook.com>
2026-03-18 17:08:50 +0100

    Refactor application bar to include menu and navigation items

--- vue_app_gestion/app/layouts/default.vue
+++ vue_app_gestion/app/layouts/default.vue
@@ -1,7 +1,21 @@
 <template>
   <v-app>
     <v-layout class="rounded rounded-md border">
-      <v-app-bar title="Application bar"></v-app-bar>
+      <v-app-bar>
+        <v-app-bar-title>Menu</v-app-bar-title>
+        <v-spacer />
+        <v-menu>
+          <template #activator="{ props }">
+            <v-btn icon="mdi-dots-vertical" variant="text" v-bind="props" />
+          </template>
+
+          <v-list>
+            <v-list-item v-for="(item, i) in items" :key="i" :value="i">
+              <v-list-item-title>{{ item.text }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </v-app-bar>
 
       <v-navigation-drawer v-model="drawer">
         <v-list nav>
@@ -32,20 +46,22 @@
             height="200"
             rounded="lg"
             width="100%"
-          ></v-sheet>
+          >
+
+          </v-sheet>
         </v-container>
       </v-main>
     </v-layout>
   </v-app>
 </template>
 
-<script setup>
+<script setup lang="ts">
   const drawer = ref(true);
 
   const items = [
     { text: 'Dashboard', icon: 'mdi-view-dashboard', url: '/' },
-    { text: 'Team', icon: 'mdi-account-group', url: "/team"  },
-    { text: 'Projects', icon: 'mdi-folder-multiple', url: "/projects"  },
+    { text: 'Team', icon: 'mdi-account-group', url: "/team" },
+    { text: 'Projects', icon: 'mdi-folder-multiple', url: "/projects" },
     { text: 'Reports', icon: 'mdi-file-chart', url: "/reports" },
   ]
 </script>
