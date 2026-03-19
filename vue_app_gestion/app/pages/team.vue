f9457a974c1de621aeefcd5f127a770e27fad923
GenXwater <louis.genty@outlook.com>
2026-03-19 14:09:38 +0100

    Add team page to display user emails dynamically

---
+++ vue_app_gestion/app/pages/team.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-container>
+        <h1>L'équipe</h1>
+        <ul>
+            <li v-for="user in usersData" :key="user.id">{{ user.email }}</li>
+        </ul>
+    </v-container>
+</template>
+
+<script setup lang="ts">
+    const { data: usersData } = await useFetch(`http://localhost:3002/users`)
+    const { data: projectsData } = await useFetch(`http://localhost:3002/projects`)
+</script>
