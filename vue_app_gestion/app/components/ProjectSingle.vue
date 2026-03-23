adaaeeb53a2a3e47177d921de8cae3e95028b6cd
GenXwater <louis.genty@outlook.com>
2026-03-23 15:52:32 +0100

    Add project listing and detail components for project management

---
+++ vue_app_gestion/app/components/ProjectSingle.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-card
+    class="mx-auto"0
+    max-width="400"
+  >
+    <v-img
+      class="align-end text-white"
+      height="200"
+      :src="getRandomImage()"
+      cover
+    >
+      <v-card-title>{{ project.name }}</v-card-title>
+    </v-img>
+
+    <v-card-subtitle class="pt-4">
+      10k€
+    </v-card-subtitle>
+
+    <v-card-text>
+      <div>{{ project.desc }}</div>
+    </v-card-text>
+
+    <v-card-actions>
+      <v-btn color="orange" text="Voir"></v-btn>
+
+      <v-btn color="orange" text="Fermer"></v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script setup lang="ts">
+    type ProjectProps = {
+        project: Object;
+    }
+
+    const props = defineProps<ProjectProps>();
+
+    const getRandomImage = () => {
+        return "https://random.imagecdn.app/300/240";
+    }
+</script>
---
+++ vue_app_gestion/app/pages/projects.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1 class="ma-0 mb-4">Liste des Projets</h1>
+    <v-container>
+        <!--- Liste de projets -->
+        <v-row>
+            <template v-for="leProjet in data"> 
+                    <v-col cols="12" md="4">
+                        <project-single :project="leProjet" />
+                    </v-col>
+
+            </template>
+        </v-row>
+    </v-container>
+    
+</template>
+
+<script setup lang="ts">
+import ProjectSingle from '~/components/ProjectSingle.vue';
+
+    const { data, status, error, refresh } = await useFetch(`http://localhost:3002/projects`)
+</script>
