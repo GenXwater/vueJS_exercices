d3c705a10455ca03fb298ca36c046680127b1c19
GenXwater <louis.genty@outlook.com>
2026-03-26 09:52:14 +0100

    feat: update package.json and vite.config.js for library build configuration, et retrait de vue pour ne pas l'importer deux fois dans l'autre projet

--- uikit/vite.config.js
+++ uikit/vite.config.js
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path '
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'UiKitPackage',
+      fileName: 'uikit-package',
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'vue',
+        },
+      },
+    },
+  },
   plugins: [vue()],
 })
