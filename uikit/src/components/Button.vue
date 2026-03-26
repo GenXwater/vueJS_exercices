2d28d84bb1ff3d050f5dd8534433e364a7dde130
GenXwater <louis.genty@outlook.com>
2026-03-26 09:58:58 +0100

    feat: add Button component with customizable styles and props

---
+++ uikit/src/components/Button.vue
@@ -0,0 +1,35 @@
+<template>
+    <button class="tuk-button" :class="type">
+        <slot>
+            Click-me
+        </slot>
+    </button>
+</template>
+
+<script setup lang="ts">
+    const props = defineProps({
+        type: {
+            type: String,
+            required: false,
+            default: "Primary"
+        }
+    })
+</script>
+
+<style>
+    .tuk-button {
+        padding: 10px 20 px;
+        border-radius: 25px;
+        border: none;
+    }
+
+    tuk-primary {
+        color: white;
+        background-color: cornflowerblue;
+    }
+
+    .tuk-secondary {
+        color: white;
+        background-color: slategray;
+    }
+</style>
