31f1d0dbc9c18797f6540f89e8a1d7fc8a2b4546
GenXwater <louis.genty@outlook.com>
2026-03-26 10:01:12 +0100

    feat: refactor main.js to export plugin installation for TukeButton component

--- uikit/src/main.js
+++ uikit/src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import Button from './components/Button.vue'
 
-createApp(App).mount('#app')
+export default {
+    install: (app: App<Element>) => {
+        app.component('TukeButton', Button)
+    }
+}
