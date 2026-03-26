dc160b58bea1e04c2b0845bf4ce6e5b4e804ec18
GenXwater <louis.genty@outlook.com>
2026-03-26 10:10:31 +0100

    feat: move main.js content to src/main.ts and update vite.config.js entry point

--- uikit/vite.config.js
+++ uikit/vite.config.js
@@ -6,7 +6,7 @@ import { resolve } from 'path '
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
+      entry: resolve(__dirname, 'src/main.ts'),
       name: 'UiKitPackage',
       fileName: 'uikit-package',
     },
